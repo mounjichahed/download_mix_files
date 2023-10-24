226e4738985dad8e7a8cc21b08bb77386b8c12ae
mchahed <mchahed@penguin>
2023-10-24 22:21:14 +0200

    modified:   .gitmodules 	modified:   download_mix_files.py 	new file:   mixes_extract_mixdb 	modified:   mixes_extract_mixdb (modified content, untracked content) 	install.sh

--- download_mix_files.py
+++ download_mix_files.py
@@ -2,25 +2,36 @@
 TOKEN="RokNHxieRWZySdOCslYJgbJCLGLOtZnVYNIZChsi"
 
 import discogs_client
-import mixesdb_extractor
+from mixes_extract_mixdb import mixesdb_extractor
 
 d = discogs_client.Client('download_mix_files/1.0', user_token=TOKEN)
 
 ltracks = mixesdb_extractor.get_list("Laurent Garnier",5)
 
-for track in ltracks:
-    if '[' in track:
-        title = track['title'].split('[')[0]
-    else:
-        title = track['title']
-    
-    results = d.search(title + " " + track['artist'])
-    print()
-    print('*****')
-    print(track)
-    print()
-    try:
-        if results.page(1)[0]:
-            print(results.page(1)[0])
-    except:
-        pass
+
+track = (ltracks[0])
+
+if '[' in track['title']:
+    track['title'] = track['title'].split('[')[0]
+
+print(track)
+
+# for track in ltracks:
+#     if(track):
+#         if '[' in track:
+#             title = track['title'].split('[')[0]
+#             if '[' in track:
+#                 title = track['title'].split('[')[0]
+#         else:
+#             title = track['title']
+        
+#         results = d.search(title + " " + track['artist'])
+#         print()
+#         print('*****')
+#         print(track)
+#         print()
+#         try:
+#             if results.page(1)[0]:
+#                 print(results.page(1)[0])
+#         except:
+#             pass
