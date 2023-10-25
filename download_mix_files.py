d3c57048a19445534e93a575b3674d8ee091c729
mchahed <mchahed@penguin>
2023-10-25 10:06:27 +0200

    modified:   .gitmodules 	modified:   README.md 	modified:   __pycache__/mixesdb_extractor.cpython-39.pyc 	modified:   download_mix_files.py 	new file:   install.sh 	modified:   mixes_extract_mixdb (modified content)

--- download_mix_files.py
+++ download_mix_files.py
@@ -3,35 +3,21 @@ TOKEN="RokNHxieRWZySdOCslYJgbJCLGLOtZnVYNIZChsi"
 
 import discogs_client
 from mixes_extract_mixdb import mixesdb_extractor
+import slskd_api
+
 
 d = discogs_client.Client('download_mix_files/1.0', user_token=TOKEN)
 
 ltracks = mixesdb_extractor.get_list("Laurent Garnier",5)
 
-
-track = (ltracks[0])
-
-if '[' in track['title']:
-    track['title'] = track['title'].split('[')[0]
-
-print(track)
-
-# for track in ltracks:
-#     if(track):
-#         if '[' in track:
-#             title = track['title'].split('[')[0]
-#             if '[' in track:
-#                 title = track['title'].split('[')[0]
-#         else:
-#             title = track['title']
-        
-#         results = d.search(title + " " + track['artist'])
-#         print()
-#         print('*****')
-#         print(track)
-#         print()
-#         try:
-#             if results.page(1)[0]:
-#                 print(results.page(1)[0])
-#         except:
-#             pass
+for track in ltracks:
+    if(track):
+        try:
+            release = d.search(track['artist'] + " - " + track['title'])[0]
+            print()
+            print('*****')
+            print(track)
+            print()
+            print(release.title)
+        except:
+            pass
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt install python3-pip
+pip3 install discogs_client bs4 slskd-api requests urllib3 argparse
