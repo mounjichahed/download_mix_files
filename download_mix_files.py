18f3422f246ade20147515a4abc473ec6b1334cf
mchahed <mchahed@penguin>
2023-10-25 10:08:35 +0200

    modified:   download_mix_files.py 	modified:   mixes_extract_mixdb (modified content)

--- download_mix_files.py
+++ download_mix_files.py
@@ -9,15 +9,22 @@ import slskd_api
 d = discogs_client.Client('download_mix_files/1.0', user_token=TOKEN)
 
 ltracks = mixesdb_extractor.get_list("Laurent Garnier",5)
+track = ltracks[5]
+release = d.search(track['artist'] + " - " + track['title'])[0]
+print()
+print('*****')
+print(track)
+print()
+print(release.title)
 
-for track in ltracks:
-    if(track):
-        try:
-            release = d.search(track['artist'] + " - " + track['title'])[0]
-            print()
-            print('*****')
-            print(track)
-            print()
-            print(release.title)
-        except:
-            pass
+# for track in ltracks:
+#     if(track):
+#         try:
+#             release = d.search(track['artist'] + " - " + track['title'])[0]
+#             print()
+#             print('*****')
+#             print(track)
+#             print()
+#             print(release.title)
+#         except:
+#             pass
